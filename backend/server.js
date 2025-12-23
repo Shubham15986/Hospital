8627140f0863869ffefc80b0fbd253ab3d3a5e99
Shubham15986 <shubham15986@gmail.com>
2025-12-23 08:01:57 +0530

    adding auth and user route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import authRoutes from './routes/authRoutes.js'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
 dotenv.config()
@@ -12,6 +14,10 @@ app.use(express.json())
 app.use(cors())
 connectDB()
 connectCloudinary()
+
+
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRouter)
 
 app.get('/',(req,res)=>{
     res.send('API is  WORKING')
